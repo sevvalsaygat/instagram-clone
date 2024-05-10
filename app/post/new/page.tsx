09f42bf659100d3a6e9343b30e49f383f0809643
sevvalsaygat <sevvallsstt@gmail.com>
2024-05-10 14:38:10 +0300

    delete: unused elements

--- app/post/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NewPost as NewPostContainer } from "@app/containers";
-
-export default function NewPost() {
-  return (
-    <div className="p-10 mt-10">
-      <NewPostContainer.Form />
-    </div>
-  );
-}
